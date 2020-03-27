902bfa428a933344055b984dfbce2fb103c5402b
guisilva9439 <guisilva9439@gmail.com>
2020-03-27 19:40:38 -0300

    Adicionado ReactJS App

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,15 +7,18 @@ const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+// Ongs Routes
 routes.get('/ongs', OngController.list);
 routes.post('/ongs', OngController.create);
 routes.delete('/delete', OngController.delete);
 
+// Incidents Routes
 routes.get('/incidents', IncidentController.list);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/profile', ProfileController.list);
-routes.post('/sessions', SessionController.create);
+// Misc
+routes.get('/profile', ProfileController.list); // List especific ong incidents
+routes.post('/sessions', SessionController.create); // Handle login
 
 module.exports = routes;
